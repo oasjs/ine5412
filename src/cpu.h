a9f5c9acb567206275514e5103b320063b67ae2b
oasjs <oasjatoba1@gmail.com>
2023-08-29 16:07:59 -0300

    Implement CPU class

--- src/cpu.h
+++ src/cpu.h
@@ -2,6 +2,8 @@
 #define CPU_H
 
 #include <list>
+#include <random>
+#include "context.h"
 #include "memory.h"
 
 /**
@@ -11,24 +13,63 @@
  */
 class CPU {
 public:
-    CPU() {}
+    CPU() {
+        registers = vector<int>(6);
+        sp = 0;
+        pc = 0;
+        st = 0;
+    }
 
     ~CPU() {}
 
-    void handlePreemption() {}
+    // Simulates the CPU changing states as it executes a process.
+    void change_state() {
+        // Prepares to generate a random number.
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_int_distribution<> dis(0, 100);
+
+        // Sets each information of the cpu state to a random number.
+        for (int n = 0; n < 6; ++n) {
+            registers[n] = dis(gen);
+        }
+        int sp = dis(gen);
+        int pc = dis(gen);
+        int st = dis(gen);
+    }
+
+    // Handles the preemption of a process.
+    void handle_preemption(int active_pid, int scheduled_pid) {
+
+        // Saves the context of the process that was running.
+        save_context(active_pid);
+
+        // Loads the context of the process that is scheduled to run. If the
+        // process is new, its context will be empty.
+        load_context(scheduled_pid);
+    }
 
 private:
     Memory memory;
-    std::list<int> registers;   // Check the type of the list
+    vector<int> registers;
     int sp; // Stack Pointer
     int pc; // Program Counter
     int st; // Status
 
-    void generateContext() {}
+    // Loads the context of a process from memory.
+    void load_context(int pid) {
+        Context context = memory.load_context(pid);
+        registers = context.get_registers();
+        sp = context.get_sp();
+        pc = context.get_pc();
+        st = context.get_st();
+    }
 
-    void loadContext() {}
-
-    void saveContext() {}
+    // Saves the context of the current process in memory.
+    void save_context(int pid) {
+        Context context(registers, sp, pc, st);
+        memory.save_context(pid, context);
+    }
 
 };
 
