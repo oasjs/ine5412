b1bb635515beb207deae48d185711392ce770ae5
oasjs <oasjatoba1@gmail.com>
2023-08-29 16:08:21 -0300

    Implement Context class

--- src/context.h
+++ src/context.h
@@ -10,14 +10,44 @@
  */
 class Context {
 public:
-    Context() {}
+
+    Context() {
+        registers = vector<int>(6);
+        sp = 0;
+        pc = 0;
+        st = 0;
+    }
+
+    Context(vector<int> registers_, int sp_, int pc_, int st_) {
+        registers = registers_;
+        sp = sp_;
+        pc = pc_;
+        st = st_;
+    }
 
     ~Context() {}
 
+    vector<int> get_registers() {
+        return registers;
+    }
+
+    int get_sp()  {
+        return sp;
+    }
+
+    int get_pc() {
+        return pc;
+    }
+
+    int get_st() {
+        return st;
+    }
+
 private:
-    std::list<int> registers;
+    vector<int> registers;
     int sp; // Stack Pointer
     int pc; // Program Counter
+    int st; // Status
 };
 
 #endif // CONTEXT_H
