bd0eb2bc58e4fb98acdf4bc4d3d31e94cc12d99c
oasjs <oasjatoba1@gmail.com>
2023-08-29 21:24:55 -0300

    Adapts main to receive command line arguments

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,19 @@
 #include "system.h"
 
-int main() {
-    System system;
-    system.start();
+int main(int argc , char** argv) {
+
+    if (argc < 2) {
+        std::cout << "Usage: ./scheduler <input_file> scheduler_type (optional)" << std::endl;
+        return 1;
+    }
+
+    // As the second argument is optional, if it is not passed, the scheduler
+    // type is set to 0, which means that all schedulers will be run.
+    int scheduler_type = 0;
+    if (argc == 3)
+        scheduler_type = atoi(argv[2]);
+
+    System system = System(argv[1]);
+    system.start(scheduler_type);
     return 0;
 }
