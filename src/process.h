c889ef3cda0bbe23882bea5b710d8c3b9e2aee9e
Joao Cordeiro <jschmidtcordeiro@gmail.com>
2023-09-05 14:08:46 -0300

    add two methods in process and the NamedType variables

--- src/process.h
+++ src/process.h
@@ -1,6 +1,19 @@
 #ifndef PROCESS_H
 #define PROCESS_H
 
+#include "../NamedType/include/named_type.hpp"
+
+using pid = NamedType<int, struct pidTag>;
+using startTime = NamedType<int, struct startTimeTag>;
+using endTime = NamedType<int, struct endTimeTag>;
+using duration = NamedType<int, struct durationTag>;
+using staticPriority = NamedType<int, struct staticPriorityTag>;
+using dynamicPriority = NamedType<int, struct dynamicPriorityTag>;
+using state = NamedType<int, struct stateTag>;
+using totalExecutionTime = NamedType<int, struct totalExecutionTimeTag>;
+using quantumExecutionTime = NamedType<int, struct quantumExecutionTimeTag>;
+
+
 /**
  * @class Process
  * @brief Represents each process and its contents.
@@ -8,20 +21,30 @@
  */
 class Process {
 public:
-    Process() {}
+    Process(pid p, startTime start, duration dur, staticPriority sPriority, dynamicPriority dPriority, state s)
+        : pid(p), startTime(start), duration(dur), staticPriority(sPriority), dynamicPriority(dPriority), state(s) {
+    }
 
     ~Process() {}
 
+    void run() {
+        totalExecutionTime++;
+    }
+
+    bool is_done() {
+        return totalExecutionTime >= duration;
+    }
+
 protected:
-    int pid;
-    int startTime;
-    int endTime;
-    int duration;
-    int staticPriority;
-    int dynamicPriority;
-    int state;
-    int totalExecutionTime;
-    int quantumExecutionTime;
+    pid pid;
+    startTime startTime;
+    endTime endTime;
+    duration duration;
+    staticPriority staticPriority;
+    dynamicPriority dynamicPriority;
+    state state;
+    totalExecutionTime totalExecutionTime;
+    quantumExecutionTime quantumExecutionTime;
 };
 
 #endif // PROCESS_H
