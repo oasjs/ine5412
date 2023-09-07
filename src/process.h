302bcf77a2e06024b85fb7b51fbc2366184de2de
dexeme <dexe.me12@gmail.com>
2023-09-07 11:53:57 -0300

    Simplify implementation

--- src/process.h
+++ src/process.h
@@ -3,25 +3,62 @@
 
 /**
  * @class Process
- * @brief Represents each process and its contents.
+ * @brief Represents a process in the system.
  *
  */
 class Process {
 public:
-    Process() {}
+    Process() {
+        pid = 0;
+        duration = 0;
+        priority = 0;
+        total_execution_time = 0;
+    }
+
+    Process(unsigned int pid_, unsigned int duration_, unsigned int priority_) {
+        pid = pid_;
+        duration = duration_;
+        priority = priority_;
+        total_execution_time = 0;
+    }
 
     ~Process() {}
 
+    /**
+     * @brief Increments the total execution time of the process and returns
+     *  true if the process has finished its execution. Otherwise returns false.
+    */
+    void run() {
+        total_execution_time++;}
+
+    bool is_done() {
+        return total_execution_time >= duration;}
+
+    /**
+     * @brief Returns the process ID.
+     */
+    unsigned int get_pid() const {
+        return pid;}
+
+    /**
+     * @brief Returns the maximum duration of the process. If the duration is
+     *  0, the process is invalid.
+     */
+    unsigned int get_duration() const {
+        return duration;}
+
+    /**
+     * @brief Returns the priority of the process. This priority is always
+     * static.
+     */
+    unsigned int get_priority() const {
+        return priority;}
+
 protected:
-    int pid;
-    int startTime;
-    int endTime;
-    int duration;
-    int staticPriority;
-    int dynamicPriority;
-    int state;
-    int totalExecutionTime;
-    int quantumExecutionTime;
+    unsigned int    pid;
+    unsigned int    duration;
+    unsigned int    priority;
+    unsigned int    total_execution_time;
 };
 
 #endif // PROCESS_H
