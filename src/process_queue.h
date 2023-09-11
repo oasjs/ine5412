2d17f493543e761531c8ec33e38ccf490a3309e6
oasjs <oasjatoba1@gmail.com>
2023-09-11 20:52:56 -0300

    Add wrapper for process queues

---
+++ src/process_queue.h
@@ -0,0 +1,80 @@
+#ifndef PROCESS_QUEUE_H
+#define PROCESS_QUEUE_H
+
+#include <queue>
+#include "process.h"
+
+
+class ProcessQueueWrapper {
+public:
+    virtual void push(Process process) = 0;
+
+    virtual Process front() = 0;
+
+    virtual void pop() = 0;
+
+    virtual bool empty() = 0;
+
+    virtual std::size_t size() = 0;
+};
+
+
+class ProcessQueue : public ProcessQueueWrapper {
+public:
+    ProcessQueue() {}
+
+    void push(Process process) {
+        queue.push(process);
+    }
+
+    Process front() {
+        return queue.front();
+    }
+
+    void pop() {
+        queue.pop();
+    }
+
+    bool empty() {
+        return queue.empty();
+    }
+
+    std::size_t size() {
+        return queue.size();
+    }
+
+private:
+    std::queue<Process> queue;
+};
+
+template<typename Comparator>
+class PriorityProcessQueue : public ProcessQueueWrapper {
+private:
+    std::priority_queue<Process, std::vector<Process>, Comparator> queue;
+
+public:
+    PriorityProcessQueue(const Comparator& c) : queue(c) {}
+
+    void push(Process process) {
+        queue.push(process);
+    }
+
+    Process front() {
+        return queue.top();
+    }
+
+    void pop() {
+        queue.pop();
+    }
+
+    bool empty() {
+        return queue.empty();
+    }
+
+    std::size_t size() {
+        return queue.size();
+    }
+
+};
+
+#endif
