83dd090af0dda16fc0daed062f1960c4809406a0
oasjs <oasjatoba1@gmail.com>
2023-09-13 17:39:15 -0300

    Remove warnings and debug print

--- src/read_file.h
+++ src/read_file.h
@@ -62,13 +62,13 @@ public:
 			processes.push_back(p);
 		}
 
-		cout << "Quantidade de processos lidos do arquivo: " << processes.size() << endl;
+		// cout << "Quantidade de processos lidos do arquivo: " << processes.size() << endl;
 	}
 
 	void print_processes_params() {
 		vector<ProcessParams *>::iterator iter = processes.begin();
 
-		for(iter; iter < processes.end(); iter++) {
+		for(; iter < processes.end(); iter++) {
 			ProcessParams *p = *iter;
 			cout << *p;
 		}
@@ -79,7 +79,7 @@ public:
     }
 
 	~File() {
-		for(int i = 0; i < processes.size() ; i++) {
+		for(size_t i = 0; i < processes.size() ; i++) {
 			ProcessParams *p = processes[i];
 			delete p;
 		}
